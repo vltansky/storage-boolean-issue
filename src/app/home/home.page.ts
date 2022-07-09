49040141fa530012d502fc27686a977996598f1c
indraraj <indraraj@infiniteit.biz>
2022-07-09 14:28:44 +0530

    feat: google map js api loader test

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,32 +1,37 @@
 /* eslint-disable no-underscore-dangle */
-import { Component, OnInit } from '@angular/core';
-import { Storage } from '@capacitor/storage';
-import { StorageService } from './storage.service';
+import { Component, AfterViewInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-  public state = false;
-  constructor(private _storageService: StorageService) { }
-
-  ngOnInit() {
-  }
-
-  async onAddStorage() {
-   this.state =  await this._storageService.LS.set('indra', true);
-  }
-
-  async onRemoveStorage() {
-    await this._storageService.LS.remove('indra');
-  }
-
-  async onGetStorage() {
-    const data = await this._storageService.LS.get('indra');
-    console.log(data);
-    this.state = data;
+export class HomePage implements AfterViewInit {
+   ngAfterViewInit() {
+    const loader = new Loader({
+      apiKey: '',
+      version: 'weekly',
+      libraries: ['places'],
+    });
+    const mapOptions = {
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+      zoom: 4,
+    };
+    loader
+      .load()
+      .then((google) => {
+        console.log(google);
+        console.log(document.getElementById('map'));
+        new google.maps.Map(document.getElementById('map'), mapOptions);
+      })
+      .catch((e) => {
+        // do something
+        console.log(e);
+      });
   }
 
 }
