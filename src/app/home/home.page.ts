40493f1bfb79d921f45914396ef1553fe8442520
indraraj <indraraj@infiniteit.biz>
2022-07-18 19:34:24 +0530

    feat: add swiper and error

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { Component, AfterViewInit } from '@angular/core';
-import SwiperCore, { Pagination } from 'swiper';
+import SwiperCore, { Pagination, SwiperOptions } from 'swiper';
 
 SwiperCore.use([Pagination]);
 
@@ -10,21 +10,17 @@ SwiperCore.use([Pagination]);
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  imageList = [];
+  isSkeletonShow = true;
+  slideMultiMobile = {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    loop: true,
+  };
 
   // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
   async ngOnInit() {
-    this.imageList = await this.resolveAfterX();
-  }
-
-  resolveAfterX(): Promise<any[]> {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve([
-          'https://img.freepik.com/free-photo/cat-white-background_155003-15381.jpg',
-          'https://img.freepik.com/premium-photo/british-shorthair-kitten-3-5-months-old-sitting-looking-up_191971-4591.jpg',
-        ]);
-      }, 2000);
-    });
+    setTimeout(() => {
+      this.isSkeletonShow = false;
+    }, 3000);
   }
 }
