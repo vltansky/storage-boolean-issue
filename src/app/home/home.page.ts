5e9ccf7d9840bc8c40b4f9cb126f17d6040d402c
Vlad Tansky <vl.tansky@gmail.com>
2022-07-21 20:04:32 +0300

    repro

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,26 +1,11 @@
 /* eslint-disable no-underscore-dangle */
-import { Component, AfterViewInit } from '@angular/core';
-import SwiperCore, { Pagination, SwiperOptions } from 'swiper';
+import { Component } from '@angular/core';
+import SwiperCore, { Pagination } from 'swiper';
 
 SwiperCore.use([Pagination]);
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
-  isSkeletonShow = true;
-  slideMultiMobile = {
-    slidesPerView: 1.2,
-    spaceBetween: 10,
-    loop: true,
-  };
-
-  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
-  async ngOnInit() {
-    setTimeout(() => {
-      this.isSkeletonShow = false;
-    }, 3000);
-  }
-}
+export class HomePage {}
